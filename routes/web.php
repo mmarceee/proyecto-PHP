a569d7de54002ea0b5f4bf47affc1b584039c9eb
mmarceee <emarcenal67@gmail.com>
2026-05-07 14:56:40 -0300

    prototipo agenda y perfil-profesional

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,11 @@ Route::get('/prototipo/busqueda', function () {
     return view('prototipos.busqueda');
 })->middleware(['auth'])->name('prototipo.busqueda');
 
+Route::get('/prototipo/agenda', function () {
+    return view('prototipos.agenda');
+})->middleware(['auth'])->name('prototipo.agenda');
+
+Route::get('/prototipo/perfil-profesional', function () {
+    return view('prototipos.perfil-profesional');
+})->middleware(['auth'])->name('prototipo.perfil-profesional');
 require __DIR__.'/auth.php';
