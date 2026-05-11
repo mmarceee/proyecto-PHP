e06f8e7a49adba6cd6aa834a6696adb5ca6ca8b3
mmarceee <emarcenal67@gmail.com>
2026-05-11 09:41:41 -0300

    Actualizacion del prototipo perfil

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ Route::view('profile', 'profile')
 
 Route::view('/prototipo/busqueda', 'prototipos.busqueda')->middleware(['auth'])->name('prototipo.busqueda');
 Route::view('/prototipo/agenda', 'prototipos.agenda')->middleware(['auth'])->name('prototipo.agenda');
-Route::view('/prototipo/perfil-profesional', 'prototipos.perfil-profesional')->middleware(['auth'])->name('prototipo.perfil-profesional');
 Route::view('/prototipo/perfil', 'prototipos.perfil')->middleware(['auth'])->name('prototipo.perfil');
 
 require __DIR__.'/auth.php';
