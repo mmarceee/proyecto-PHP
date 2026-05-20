66366872988e58e72c44545f38a15cd77e77e17f
lukovski <lucasottonellodolio@gmail.com>
2026-05-20 10:06:21 -0300

    Agrego lang y saco comentarios

--- app/Http/Controllers/Api/ProfileApiController.php
+++ app/Http/Controllers/Api/ProfileApiController.php
@@ -13,22 +13,18 @@ class ProfileApiController extends Controller
 {
     public function updateInfo(Request $request, ProfileService $profileService)
     {
-        // 1. El Mozo toma y revisa el pedido
         $validated = $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique('users')->ignore($request->user()->id)],
         ]);
 
-        // 2. El Mozo le pasa el pedido al Chef (El Servicio)
         $user = $profileService->updateInformation($request->user(), $validated);
 
-        // 3. El Mozo devuelve el plato listo en formato JSON
        return response()->json([
             'mensaje' => 'Perfil actualizado exitosamente',
             'usuario' => [
                 'nombre' => $user->name,
                 'correo' => $user->email
-                // Solo mandamos esto. Ni el ID, ni fechas de creación, nada extra.
             ] 
         ]);
     }
