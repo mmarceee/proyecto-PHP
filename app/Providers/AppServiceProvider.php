4046d414a6d8c79adf82b19e6548a54160da5e1d
Cristian Reyes <cristian.reyes@estudiantes.utec.edu.uy>
2026-06-23 17:51:33 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,17 +12,11 @@ use App\Services\EventLogService;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
 
